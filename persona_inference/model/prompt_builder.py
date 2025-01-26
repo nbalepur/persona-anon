0f8475c849e91367ff2d282697bfb3fb9cfe614e
nbalepur <nbalepur@umd.edu>
2025-01-26 12:38:04 -0500

    fixes

--- persona_inference/model/prompt_builder.py
+++ persona_inference/model/prompt_builder.py
@@ -8,14 +8,14 @@ class PromptBuilder:
 
     def get_prompts(self, prompt_type: PromptType):
 
-        # template for the prompt
-        prompt_factory = PromptFactory(args=self.args, prompt_type=prompt_type)
-        prompt_parser = prompt_factory.get_prompt(prompt_type)
-
         # data inputs used to infill the prompt template
         data_factory = DataFetcherFactory()
         data_fetcher = data_factory.get_data_fetcher(prompt_type=prompt_type, args=self.args)
         prompt_inputs = data_fetcher.get_data()
+
+        # template for the prompt
+        prompt_factory = PromptFactory(args=self.args, prompt_type=prompt_type)
+        prompt_parser = prompt_factory.get_prompt(prompt_type)
 
         prompts = [None if p == None else prompt_parser.create_prompt(**p) for p in prompt_inputs]
         return prompts
--- persona_inference/scripts/build_data.sh
+++ persona_inference/scripts/build_data.sh
@@ -3,9 +3,10 @@
 # source ... -> activate your conda environment
 conda activate persona
 
-dataset_name="" # original dataset
 run_name="default" # run name of personas
 model="gpt-4o-mini" # model that generated personas
+
+dataset_name="" # original dataset
 inference_split="BeaverTails" # inference split used to create personas
 new_dataset_name="BeaverTails_with_Persona" # new name of the dataset
 
